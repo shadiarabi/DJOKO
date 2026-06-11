0038e572f14fbbdd70290669b1cf5fb80c6909f3
shadiarabi <47791264+shadiarabi@users.noreply.github.com>
2026-06-11 14:29:36 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  
-  const url = process.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL || '';
-  const key = process.env.VITE_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY || '';
-  
-  res.status(200).json({ supabaseUrl: url, supabaseKey: key });
-}
